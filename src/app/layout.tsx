d6c59fad5bdc6d2c9247749c1d9bdce7e7d1fe09
sameera222 <sameeraanjumh@gmail.com>
2023-03-20 20:50:20 +0530

    added sidebar to the screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import SideBar from './components /SideBar';
+import './globals.css';
 
 export const metadata = {
   title: 'Chatgpt Clone',
@@ -14,7 +15,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className='flex'>
-          {/* SideBar  */}
+          <div  className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+          <SideBar/>
+          </div>
           {/* ClientProvider-Notification  */}
           <div className='bg-[#343541] flex-1'>
           {children}
