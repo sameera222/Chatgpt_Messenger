932e9458b9c51dc2e00ba5ae124f8055a18f8734
sameera222 <sameeraanjumh@gmail.com>
2023-03-27 15:27:08 +0530

    implement react hot toast library for notification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { authOptions } from '../../pages/api/auth/[...nextauth]';
 import { SessionProvider } from './components /SessionProvider';
 import Login from './components /Login';
+import ClientProvider from './components /ClientProvider';
 
 export const metadata = {
   title: 'Chatgpt Clone',
@@ -28,7 +29,8 @@ export default async function RootLayout({
           <div  className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
           <SideBar/>
           </div>
-          {/* ClientProvider-Notification  */}
+
+          <ClientProvider/>
           <div className='bg-[#343541] flex-1'>
           {children}
           </div>
